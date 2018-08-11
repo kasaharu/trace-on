399648413f87e319b155c724364465fdcb306932
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-11 16:05:47 +0900

    MogmogComponent を作成 by CLI command

---
+++ src/app/components/mogmog/mogmog.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MogmogComponent } from './mogmog.component';
+
+describe('MogmogComponent', () => {
+  let component: MogmogComponent;
+  let fixture: ComponentFixture<MogmogComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MogmogComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MogmogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/mogmog/mogmog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mogmog',
+  templateUrl: './mogmog.component.html',
+  styleUrls: ['./mogmog.component.scss']
+})
+export class MogmogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
