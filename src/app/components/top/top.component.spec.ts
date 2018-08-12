d24cff43e86ecc6c8870b32c0b014faf9330fcf5
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-12 20:41:26 +0900

    welcome メッセージの表示場所を AppComponent から TopComponent に変更

--- src/app/components/top/top.component.spec.ts
+++ src/app/components/top/top.component.spec.ts
@@ -22,4 +22,15 @@ describe('TopComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as title 'app'`, async(() => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
 });
--- src/app/components/top/top.component.ts
+++ src/app/components/top/top.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top.component.scss'],
 })
 export class TopComponent implements OnInit {
+  title = 'app';
 
   constructor() { }
 
