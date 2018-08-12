1f2594b245ab57d49551e54aea6d8b83997a172f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-12 10:13:14 +0900

    TopComponent と MogmogComponent へのルーティング追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TopComponent } from './components/top/top.component';
+import { MogmogComponent } from './components/mogmog/mogmog.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/top', pathMatch: 'full' },
+  { path: 'top', component: TopComponent },
+  { path: 'top', component: TopComponent },
+  { path: 'mogmog', component: MogmogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
