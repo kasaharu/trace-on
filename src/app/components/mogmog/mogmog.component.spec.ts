01615ab3cbca6c600b8b6294a42381665cc30294
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-13 09:29:47 +0900

    MogmogComponent から MogmogHeaderComponent を呼び出し

--- src/app/components/mogmog/mogmog.component.spec.ts
+++ src/app/components/mogmog/mogmog.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MogmogComponent } from './mogmog.component';
+import { MogmogHeaderComponent } from '../mogmog-header/mogmog-header.component';
 
 describe('MogmogComponent', () => {
   let component: MogmogComponent;
@@ -8,7 +9,7 @@ describe('MogmogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MogmogComponent ],
+      declarations: [MogmogComponent, MogmogHeaderComponent],
     })
     .compileComponents();
   }));
