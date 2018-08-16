498210b2cc0ea076574e33ef194e46ad9aff6da7
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-16 10:35:15 +0900

    MogmogComponent から MogmogPopularMenuListComponent を呼び出し

--- src/app/components/mogmog/mogmog.component.spec.ts
+++ src/app/components/mogmog/mogmog.component.spec.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MogmogComponent } from './mogmog.component';
 import { MogmogHeaderComponent } from '../mogmog-header/mogmog-header.component';
 import { MogmogCoverImageComponent } from '../mogmog-cover-image/mogmog-cover-image.component';
+import { MogmogPopularMenuListComponent } from '../mogmog-popular-menu-list/mogmog-popular-menu-list.component';
 
 describe('MogmogComponent', () => {
   let component: MogmogComponent;
@@ -11,7 +12,12 @@ describe('MogmogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MogmogComponent, MogmogHeaderComponent, MogmogCoverImageComponent],
+      declarations: [
+        MogmogComponent,
+        MogmogHeaderComponent,
+        MogmogCoverImageComponent,
+        MogmogPopularMenuListComponent,
+      ],
       imports: [FontAwesomeModule],
     })
     .compileComponents();
